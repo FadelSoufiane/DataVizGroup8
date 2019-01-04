5b19ef2071e32f1de969c01697b670896213f35c
EL Bouti Mohamed <mohamed.el-bouti@etu.univ-lyon1.fr>
2019-01-04 02:27:22 +0100

    fix js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 var margin = {
         top: 10,
-        right: 10,
+        right: 100,
         bottom: 20,
-        left: 30
+        left: 100
     },
     width = 920 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;

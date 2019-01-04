10ef0bf77a3d6a577b74250f067ebe418971ede7
EL Bouti Mohamed <mohamed.el-bouti@etu.univ-lyon1.fr>
2019-01-04 02:16:30 +0100

    fix js

--- js/index.js
+++ js/index.js
@@ -82,10 +82,9 @@ d3.csv("data.csv", type, function(error, data) {
         .on("mouseover", function(d){
              //alert("Year: " + d.Year + ": " + d.Celsius + " Celsius");
             d3.select("#_yr")
-                .text("Date :   " + d.Year +"  "  );
-            "  "
-            d3.select("#degrree")
-                .text("      . "+d.Celsius + "°C");
+                .text("Date :   " + d.Year +"  "  +d.Celsius + "°C" );
+
+
         });
 
     svg.append("g")
